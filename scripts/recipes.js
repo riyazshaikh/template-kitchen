b08bcc110f76f460ff94c5f92aca8fe5fc767adf
Riyaz Shaikh <dev@katana-software.com>
2015-11-23 11:44:42 -0600

    move recipes to template-kitchen site for now Don't think we need need overflow-hidden

--- scripts/recipes.js
+++ scripts/recipes.js
@@ -29,27 +29,7 @@ RECIPES.alfa = { // parallax header/footer - regular page (use as support page)
 		"alt-headingColor": "#fff",
 
 		"banner-bgColor": "rgb(16,17,19)",
-		"banner-textColor": "#fff",
-
-		"Header - Display": "Overlaid",
-		"Header - Background": "Transparent",
-		"Header  Content - Layout": "Alternate",
-		"Header  Block - Display": "Hide",
-		"Header  Nav - Display": "Auto",
-
-		"Banner - Background": "Transparent",
-		"Banner  Image - Parallax": "Enable",
-		"Banner  Description - Display": "Hide",
-		"Banner  Blocks - Background": "Translucent",
-		"Banner  Blocks  Content - Layout": "Banner",
-
-		"Page Footer - Palette": "Banner",
-		"Page Footer  Image - Parallax": "Enable",
-		"Page Footer  Content - Layout": "Banner",
-		"Page Footer  Content - Sticky": "within-site-bottom",
-		"Page Footer  Blocks - Background": "Translucent",
-
-		"Footer - Display": "Hide",
+		"banner-textColor": "#fff"
 	},
 	webfontsJson: [
 		
@@ -107,21 +87,7 @@ RECIPES.charlie = { // scrolling gallery - custom gallery - Katana
 	tweakJson: {
 		"main-bgColor": "#fff",
 		"main-textColor": "#323639",
-		"main-headingColor": "#000",
-
-		"Header - Palette": "Main",
-		"Header - Background": "Transparent",
-		"Header - Display": "Overlaid",
-		"Header - Sticky": "Within Site",
-		"Header  Content - Layout": "Alternate",
-		"Header  Block - Display": "Hide",
-		"Header  Nav - Display": "Hide",
-
-		"Banner - Palette": "Main",
-		"Banner  Image - Parallax": "Enable",
-		"Banner  Description - Display": "Hide",
-		"Banner  Blocks  Content - Layout": "Main",
-		"Banner  Blocks  Content - Parallax": "Enable"
+		"main-headingColor": "#000"
 	},
 	webfontsJson: [
 		
@@ -130,13 +96,6 @@ RECIPES.charlie = { // scrolling gallery - custom gallery - Katana
 
 RECIPES.delta = { // fixed sliding gallery - Index - 
 	tweakJson: {
-		"Banner - Display": "Overlaid",
-		"Banner - Background": "Transparent",
-		"Banner - Palette": "Main",
-		"Banner  Blocks  Content - Layout": "Main",
-		"Banner  Blocks  Content - Italic": "Highlight",
-
-		"Gallery  Content - Layout": "Banner"
 	},
 	webfontsJson: [
 		
@@ -145,22 +104,6 @@ RECIPES.delta = { // fixed sliding gallery - Index -
 
 RECIPES.echo = { // product grid - Products
 	tweakJson: {
-		"Header  Nav - Display": "Hide",
-		"Header - Palette": "Main",
-		"Header  Content - Layout": "Alternate",
-
-		"Banner - Background": "Transparent",
-		"Banner - Palette": "Main",
-		"Banner  Blocks  Content - Layout": "Main",
-		"Banner  Blocks  Content - Italic": "Dash",
-		"Banner  Description  Title - Display": "Hide",
-
-		"Products  Category Nav - Sticky": "Within Site",
-
-		"Product Item Size": "2:3 Standard (Vertical)",
-		"Product Image Auto Crop": "False",
-		"Products  Content - Layout": "Alternate",
-		"Products  Product Meta - Flex": "Space Between"
 	},
 	webfontsJson: [
 		
