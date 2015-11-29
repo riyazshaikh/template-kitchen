08fcdeac3e25b93cc501c6a11e9b33ecdd727e4e
Riyaz Shaikh <dev@katana-software.com>
2015-11-29 12:17:16 -0600

    recipe refine

--- scripts/recipes.js
+++ scripts/recipes.js
@@ -96,7 +96,7 @@ RECIPES.echo = { // product grid - Products
 	tweakJson: {
 		"main-bgColor": "#f3efee",
 		"main-metaColor": "#767676",
-		"mainMetaWidth": "45%",
+		"mainMetaWidth": "500px",
 
 		"body-font": "{font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;font-size:14px;font-weight:400;line-height:1.5em;}",
 		"heading1-font": "{font-family:'Brandon',Helvetica,Arial,sans-serif;font-size:42px;font-weight:500;line-height:1.3em;}",
