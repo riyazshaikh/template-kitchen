519fabf64f2da119c6b95f819f35dc3756cd98ad
Riyaz Shaikh <dev@katana-software.com>
2016-01-05 12:32:10 -0600

    st

--- scripts/tweaking.js
+++ scripts/tweaking.js
@@ -15,11 +15,45 @@ Y.use('node', function (Y) {
 
 			Y.Global.on('tweak:change', function (f) {
 				try {
-					setTweak(f.getName(), f.getValue());	
+					console.log(f.config);
 				} catch(e) {
 					console.log('error in tweaking.js', e);
 				}
 			});
+
+			var updateRecipe = function() {
+				var re = /tweak:(.*?recipe:.*?)\n/g;
+				var matches = [];
+				var str = window.top.Y.Squarespace.TweakManager.rawLess;
+				var recipes = [];
+
+				while((matches = re.exec(str)) !== null) {
+						var obj = eval('('+matches[1]+')');
+						recipes.push({
+							target: obj.target,
+							properties: obj.properties,
+							condition: obj.showOnlyWhenPresent
+						});
+					}
+				});
+
+				str = "<script id='recipes'>console.log('recipes',"+JSON.stringify(recipes)+");</script>";
+
+				Y.Data.get({
+					url: '/api/config/GetInjectionSettings',
+					success: function(data) {
+						data.header = str + data.header.replace("<script id='recipes'.*?script>", '');
+
+						Y.Data.post({
+							url: '/api/config/SaveInjectionSettings',
+							data: data,
+							success: function() {
+								console.log('injection updated');
+							}
+						});
+					}
+				})
+			};
 
 			Y.Global.on('tweak:save', function (f) {
 				// var worker = new Worker("/scripts/recipe-worker.js");
@@ -29,18 +63,11 @@ Y.use('node', function (Y) {
 
 				// worker.postMessage(window.top.Y.Squarespace.TweakManager.rawLess);
 				// console.log('message sent');
-
-				window.top.Y.Squarespace.TweakManager.rawLess.split('\n').forEach(function (line) { 
-					var words = line.split("tweak:");
-					if (words[1]) {
-						var tweak = eval('('+words[1]+')');
-						console.log(tweak);
-					}
-				});
+				updateRecipe();
 			});
 
 			Y.Global.on('tweak:reset', function (f) {
-				window.RecipeManager.resetTweaks();
+				updateRecipe();
 			});
 
 		}
