d2b951fd8615c8f2e7e5cd9dfc33afbe93ffb599
Riyaz Shaikh <dev@katana-software.com>
2016-01-06 15:47:44 -0600

    refactor alfa

--- scripts/tweaking.js
+++ scripts/tweaking.js
@@ -27,7 +27,10 @@ Y.use('node', function (Y) {
 						var obj = eval('('+matches[1]+')');
 
 						obj.properties = obj.properties || [obj.property]; // convert single property to array
-						obj.properties[0] = obj.properties[0] + "=" + values[obj.title].toLowerCase().replace(/\s/g,"-");
+
+						if (values[obj.title]) {
+							obj.properties[0] = obj.properties[0] + "=" + values[obj.title].toLowerCase().replace(/\s/g,"-");	
+						}
 
 						recipes.push({
 							target: obj.target,
